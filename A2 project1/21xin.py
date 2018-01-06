4f8fb536bc1b71b2e0d3e8729c65ef7d2a6ff8f6
Macbook Air1 <Sandy@MacbookdeMacBook-Air.local>
2018-01-06 15:16:28 +0800

    add a study note document

--- A2 project1/21xin.py
+++ A2 project1/21xin.py
@@ -1,67 +1,28 @@
-import random
 
-def name():
-    name1=input('Enter player 1 username: ')
-    name2=input('Enter player 2 username: ')
-    return name1, name2
+import math
 
-def display(name,n):
-    if n==1:
-        n='Ace'
-    if n==11:
-        n='Jack'
-    if n==12:
-        n='Queen'
-    if n==13:
-        n='King'
+
+class Solver:
+
+def demo(self, a, b, c):
+    d = b ** 2 - 4 * a * c
+    if d > 0:
+        disc = math.sqrt(d)
+        root1 = (-b + disc) / (2 * a)
+        root2 = (-b - disc) / (2 * a)
+        return root1, root2
+    elif d == 0:
+        return -b / (2 * a)
     else:
-        n=str(n)
-    print(name,'gets a',n)
+        return "This equation has no roots"
 
-def process(name):
-    n=random.randrange(1,14)
-    display(name,n)
-    if n>10:
-        return 10
-    else:
-        return n
 
-def thisplayerend(name,total):
-    print('')
-    print('---------[Player:',name,']---------')
-    if total>21:
-        return True
-    if continueend(name,total)==False:
-        return True
-    return False
+if __name__ == '__main__':
+    solver = Solver()
 
-def continueend(name,total):
-    print('Total:',total)
-    choice=input('Do you want to continue or end?')
-    if choice=='end':
-        return False
-    return True
-    
-def thisplayerturn(name):
-    print('Pass the device to', name)
-    total=0
-    while thisplayerend(name,total)==False:
-        n=process(name)
-        total=total+n
-    if total>21:
-        total=total-1
-    return total
-
-def main():
-    name1,name2=name()
-    score1=thisplayerturn(name1)
-    print('')
-    score2=thisplayerturn(name2)
-    if score1==score2:
-        print('Player',name1,'and Player',name2, 'tie!')
-    if score1>score2:
-        print(name1,'wins')
-    if score1<score2:
-        print(name2,'wins')
-
-main()
+while True:
+    a = int(input("a: "))
+    b = int(input("b: "))
+    c = int(input("c: "))
+    result = solver.demo(a, b, c)
+    print(result)
